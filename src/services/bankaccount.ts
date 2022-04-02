c24a130392099dc6f0d78a3cc7299b4723167924
mrpsycox <silvestri.1699926@studenti.uniroma1.it>
2022-04-02 20:48:22 +0200

    added sum in incomes

--- src/services/bankaccount.ts
+++ src/services/bankaccount.ts
@@ -20,9 +20,22 @@ export default class BankAccountService {
         return created_ats; 
     }
 
-    private getBalancefromAggregate(obj: Object){
-
+    private getBalancefromAggregate(obj:Object){
+        for(let x of Object.values(obj)){
+            return x.transactions.balance;
+        }
     }
+
+    private listResponseObject(name: String,created_at: Date,balance: Number,total_income: Number, total_outgo: Number){
+        return {
+            name: name,
+            bankaccount_creation: created_at,
+            balance: balance,
+            total_income: total_income,
+            total_outgo: total_outgo
+        }
+    }
+
 
 
     public async create(name: String, owner: String, type: String): Promise<any> {
@@ -65,14 +78,18 @@ export default class BankAccountService {
                 
                 if(bankAccountNames.length > 1 && bankAccountCreations.length > 1){
 
-                    console.log("sono qui")
-                    const company_pvt = await (await BankAccountModel.aggregate().match({type:'private' , owner: company}).unwind('transactions')).slice(-1);
-                    const company_pub = await (await BankAccountModel.aggregate().match({type:'public' , owner: company}).unwind('transactions')).slice(-1);
+                    const lastbalance_pvt = await (await BankAccountModel.aggregate().match({type:'private' , owner: company}).unwind('transactions')).slice(-1);
+                    const lastbalance_pub = await (await BankAccountModel.aggregate().match({type:'public' , owner: company}).unwind('transactions')).slice(-1);
 
-                    console.log(company_pvt)
-                    console.log(company_pub);
-                    const balance_pvt = this.getBalancefromAggregate(company_pvt);
-                    const balance_pub = this.getBalancefromAggregate(company_pub);
+                    const balance_pvt = this.getBalancefromAggregate(lastbalance_pvt);
+                    const balance_pub = this.getBalancefromAggregate(lastbalance_pub);
+
+                    const balance_aggregate = balance_pvt+balance_pub;
+
+                    const total_incomepvt_object = await BankAccountModel.aggregate().match({type:'private', owner: company}).unwind('transactions').group({ '_id':'_id', 'totalincome':{ '$sum':'$transactions.income'}});
+                    
+                    console.log(total_incomepvt_object)
+                    
                 }
 
                 /*
@@ -80,7 +97,6 @@ export default class BankAccountService {
                 const company_pub = await (await BankAccountModel.aggregate().match({type:'public' , owner: company}).unwind('transactions')).slice(-1);
                 */
                 
-                console.log("sono li")
                 return {
                     stuff: true,
                 }
